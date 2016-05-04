da65b2cf58583bfc1be8157b650f89c1b3e5bfa0
Richard Dickinson <richard@dickinson350.freeserve.co.uk>
2016-05-04 23:23:02 +0100

    Update Hello GITHUB.c

--- Hello GITHUB.c
+++ Hello GITHUB.c
@@ -21,5 +21,10 @@ return 0;
 and it starts the program with this as the first function to be run. Without a main function, a C program cannot start or run! 
 Here the main function contains a printf statement that will run the printf function, to add the message 
 "Hello GITHUB" to the screen. \n is a format specifier escape sequence to add a newline.
+
+The main function is designated as an "int" type. This means it returns an integer value. It as "void" as it's parameter arguments
+which basically means the parameter arguments list is empty (which is the meaning of void). Functions can contain several parameter 
+arguments (void is just one possibility).
+
 return 0; directs the function to return the value 0 to the operating system which effectively means the program completes 
 successfully. Most C compilers require this. This is the usual first C program to start to learn C programming with!*/
